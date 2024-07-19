805e4b95e47536259bbc22d8c77c88d4c0de91f0
Chris Sewell <chrisj_sewell@hotmail.com>
2024-07-19 22:33:27 +0200

    update to v0.0.6

---
+++ update_content.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import shutil
+
+
+exclude = ['update_content.py', '.gitignore', '.git', '.nojekyll']
+
+for path in Path(__file__).parent.iterdir():
+    if path.name in exclude:
+        continue
+    if path.is_dir():
+        shutil.rmtree(path)
+    else:
+        path.unlink()
+
+
+source = "../sphinx-lsp/docs/_build"
+
+for path in Path(source).iterdir():
+    if path.name in exclude:
+        continue
+    if path.is_dir():
+        shutil.copytree(path, Path(__file__).parent / path.name)
+    else:
+        shutil.copy(path, Path(__file__).parent)
